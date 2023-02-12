b4b3b54fde0027ff9220dc76ebc34004ba188cf9
Tomas <tywe@kth.se>
2023-02-12 15:16:29 +0100

    find right endpoints not done

--- src/dishSource.js
+++ src/dishSource.js
@@ -1,27 +1,48 @@
+//use this endpoint for getMenuDetails --> GET Recipe information Bulk 
+//use this endpoint for searchDishes --> GET Search recipes
 import { API_KEY, BASE_URL } from "./apiConfig";
 
+function processHTTPResponseACB(response){
+    response.json();
+}
 
-function onResponseSuccessACB(response){
-    console.log(response.json());
+function processHTTPContentACB(array){
+  if(array.length>0)  
+    return array[0];
 }
 
 function onResponseFailureACB(error){
-    console.log(error);
+  console.log(error);
 }
 
+const API_ENDPOINT_1 = 'recipes/informationBulk';
+const API_ENDPOINT_2 = 'recipes/complexSearch';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-Mashape-Key': API_KEY,
-		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 	}
 };
 
-function getMenuDetails(array){
+function getMenuDetails(array) {
+  const param = array.join();
+  fetch(BASE_URL+API_ENDPOINT_1+'?ids='+param, options)
+  .then(processHTTPResponseACB)
+  .catch(onResponseFailureACB);
   
-  console.log(array)
-  fetch(BASE_URL , options).then(onResponseSuccessACB)
-  .catch(onResponseFailureACB);   
+  //.catch(onResponseFailureACB);
 }
 
-export {getMenuDetails};
+function getDishDetails(id) {
+  //const array = [id];
+  getMenuDetails([id]).then(processHTTPContentACB);
+}
+
+function searchDishes(object) {
+  console.log(object.type);
+  const param = new URLSearchParams("?type="+object.type+"&query="+object.query);
+  fetch(BASE_URL+API_ENDPOINT_2+param, options)
+  .then(onResponseSuccessACB);
+}
+
+export {getMenuDetails,getDishDetails,searchDishes};
