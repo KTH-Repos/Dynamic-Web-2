fa5b46a90254b36e8e2e754a629dfd171fe4bfa4
Tomas <tywe@kth.se>
2023-02-12 17:51:25 +0100

    add updated endpoints

--- src/dishSource.js
+++ src/dishSource.js
@@ -3,16 +3,20 @@
 import { API_KEY, BASE_URL } from "./apiConfig";
 
 function processHTTPResponseACB(response){
-    response.json();
+    if(response.status!==200)
+      throw (`Error: ${response.status}`)
+    return response.json();
 }
 
-function processHTTPContentACB(array){
-  if(array.length>0)  
-    return array[0];
+function processHTTPContentACB(response){
+  const bool = Array.isArray(response);
+  console.log(bool);
+  console.log(response);
+  return response;
 }
 
-function onResponseFailureACB(error){
-  console.log(error);
+function onResponseFailureACB(err){
+  console.error(err);
 }
 
 const API_ENDPOINT_1 = 'recipes/informationBulk';
@@ -28,19 +32,19 @@ function getMenuDetails(array) {
   const param = array.join();
   fetch(BASE_URL+API_ENDPOINT_1+'?ids='+param, options)
   .then(processHTTPResponseACB)
+  .then(processHTTPContentACB)
   .catch(onResponseFailureACB);
-  
-  //.catch(onResponseFailureACB);
 }
 
 function getDishDetails(id) {
   //const array = [id];
-  getMenuDetails([id]).then(processHTTPContentACB);
+  return getMenuDetails([id]);
+  
 }
 
 function searchDishes(object) {
   console.log(object.type);
-  const param = new URLSearchParams("?type="+object.type+"&query="+object.query);
+  const param = new URLSearchParams(`query=${encodeURIComponent(object.query)}&type=${encodeURIComponent(object.type)}`);
   fetch(BASE_URL+API_ENDPOINT_2+param, options)
   .then(onResponseSuccessACB);
 }
