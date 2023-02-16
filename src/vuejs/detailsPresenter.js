10052f5d0fe336e00b327f8bbfe8384fe5f26804
Rafael Bechara <75490904+raeef96@users.noreply.github.com>
2023-02-16 21:34:25 +0100

    Update detailsPresenter.js

--- src/vuejs/detailsPresenter.js
+++ src/vuejs/detailsPresenter.js
@@ -1,19 +1,19 @@
 
 import DetailsView from '../views/detailsView.js';
-import { getDishDetails } from '../dishSource.js';
+import promiseNoData from '../views/promiseNoData.js';
 
 
 
 
 function Details(props){
 
-   
-        
+    console.log(props);
+    
     
     return <DetailsView guests = {props.model.numberOfGuests} dishData = {props.model.currentDishPromiseState.data} />
     
 
-    
+   
 }
 
 
