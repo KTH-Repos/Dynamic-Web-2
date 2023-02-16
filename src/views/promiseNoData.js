aab9b6d766698c9b96d9564ce94161104a6dd50d
Rafael Bechara <75490904+raeef96@users.noreply.github.com>
2023-02-16 17:45:06 +0100

    done with tw2.4

--- src/views/promiseNoData.js
+++ src/views/promiseNoData.js
@@ -1,15 +1,32 @@
-
-function promiseNoData(props){
-    
-    
-    const newData = JSON.parse(JSON.stringify(props));
-    
-    return(
-        <div>
-          {promiseNoData(newData)}
-        </div>
+function promiseNoData(props) {
+  console.log(props.error)
+  if (!props || !props.promise) {
+    return (
+      <div>
+        No Data
+      </div>
     )
+  }
 
+  if (props.promise && !props.data && !props.error) {
+    return (
+      
+      <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" className="hello" />
+      
+    )
+  }
+
+  if (props.promise && !props.data && props.error) {
+    return (
+      <div>
+        dummy error to show
+      </div>
+    )
+  }
+
+  if (props.promise && props.data && !props.error) {
+    return false;
+  }
 }
 
 export default promiseNoData;
