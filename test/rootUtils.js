32db8904fec02396db6dc6ff0b54fac12801f73b
Rafael Bechara <75490904+raeef96@users.noreply.github.com>
2023-02-28 16:41:24 +0100

    Revert "fix tests"
    This reverts commit b949252715342e58e40d2f014600d3dc1edbefea.

--- test/rootUtils.js
+++ test/rootUtils.js
@@ -56,10 +56,8 @@ async function testVue(theTest){
         app.use(VueRootAll.router);
         app.mount(div);
     });
-    const result= await theTest(propsHistory);
+    await theTest(propsHistory);
     app.unmount();
-    if(result===false)
-        return false;
     return true;
 }
 
@@ -89,10 +87,8 @@ async function testReact(theTest){
         require("react-dom").render(<Guard/>, div);
     });
 
-    const result= await theTest(propsHistory);
+    await theTest(propsHistory);
     turnOff();
-    if(result===false)
-        return false;
     return true;
 }
 
@@ -123,12 +119,9 @@ async function testRoutes(propsHistory){
 
 async function testSuspense(propsHistory){
     const {state, findPersistencePropNames}=require("./mockFirebase.js");
-    const x= findPersistencePropNames();
-    if(!x)
-        return false;
-    const {numberOfGuests, dishes, currentDish}= x;
+    const {numberOfGuests, dishes, currentDish}= findPersistencePropNames();
     
-    expect(propsHistory.length, "Root should render the initial promise: first no data, then an image, then the app").to.be.gte(3);
+    expect(propsHistory.length, "Root should render other components").to.be.gte(3);
     expect(propsHistory[0].type, "the root component will briefly render 'no data' before the lifecycle executes").to.equal("no data");
     expect(propsHistory[1].type, "the root component will render a loading image (suspense) while the persistence promise is resolved").to.equal("imgLoader");
     expect(propsHistory[2].model?.numberOfGuests, "the model passed to the presenters contains the data from the cloud (nr guests)").to.equal(state.data[numberOfGuests]);
